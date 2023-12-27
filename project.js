b53b16e2065c7093f55a515f29e9e9625af31d6c
Chirag Garg <102269022+cgarg3@users.noreply.github.com>
2023-12-27 08:25:23 +0000

    added line function

--- project.js
+++ project.js
@@ -24,5 +24,24 @@ function deposit() {
 }
 }
 
+function getNumberOfLines(){
+ // eslint-disable-next-line no-constant-condition
+ while(true){
+    const lines = prompt("Enter the number of lines to bet on (1-3): ");
+    const numberOfLines = parseFloat(lines);
+
+    if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines >= 3) {
+        console.log("Invalid number of lines, try again.");
+    }
+    else {
+        return numberOfLines;
+    }
+}
+}
+
+
 const depositAmount = deposit();
 console.log(depositAmount);
+
+const lines = getNumberOfLines();
+console.log(lines);
